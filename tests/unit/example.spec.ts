566fbffd736dea684a75916973e8e280255a04b6
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2018-09-29 14:00:05 +0200

    Fix tests and add circle ci config

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,31 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoItem from "../../src/components/TodoItem.vue";
+import {Todo} from "@/Models";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("TodoItem.vue", () => {
+  it("renders none completed", () => {
+    const wrapper = shallowMount(TodoItem, {
+      propsData: {
+       todo: {
+         id: "1",
+         title: "todo",
+         completed: false
+       } as Todo
+    }});
+    expect(wrapper.findAll('div').at(1).text()).toEqual("todo");
+    expect(wrapper.findAll('div').at(1).classes()).toHaveLength(1);
+  });
+
+  it("renders completed", () => {
+    const wrapper = shallowMount(TodoItem, {
+      propsData: {
+        todo: {
+          id: "1",
+          title: "todo",
+          completed: true
+        } as Todo
+      }});
+    expect(wrapper.findAll('div').at(1).text()).toEqual("todo");
+    expect(wrapper.findAll('div').at(1).classes()).toHaveLength(2);
   });
 });
