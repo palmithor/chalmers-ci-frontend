d6afcbe3021f16c103e2802ebb966843afa722ab
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2018-09-29 14:34:01 +0200

    Fix gcloud setup

--- install_gcloud.sh
+++ install_gcloud.sh
@@ -2,4 +2,4 @@
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 sudo echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
+sudo apt-get update && sudo apt-get --only-upgrade install kubectl google-cloud-sdk google-cloud-sdk-app-engine-grpc google-cloud-sdk-pubsub-emulator google-cloud-sdk-app-engine-go google-cloud-sdk-cloud-build-local google-cloud-sdk-datastore-emulator google-cloud-sdk-app-engine-python google-cloud-sdk-cbt google-cloud-sdk-bigtable-emulator google-cloud-sdk-app-engine-python-extras google-cloud-sdk-datalab google-cloud-sdk-app-engine-java
