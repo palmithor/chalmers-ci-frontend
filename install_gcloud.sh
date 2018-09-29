ff867310e221b649052d4761e87a9929f68f1b2a
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2018-09-29 14:20:27 +0200

    Fix gcloud installation file.

--- install_gcloud.sh
+++ install_gcloud.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-sudo echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo apt-get update -y && apt-get install google-cloud-sdk -y
+sudo echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
